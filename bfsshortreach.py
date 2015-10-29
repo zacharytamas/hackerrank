842e2abff16f4a49c0c432a7cc8bab67b5f07164
Zachary Jones <zacharytamas@gmail.com>
2015-10-29 11:52:07 -0400

    First submission!

--- bfsshortreach.py
+++ bfsshortreach.py
@@ -27,44 +27,112 @@ def allocateEdges(graph, edges):
   >>> graph = [[], [], [], []]
   >>> edges = [(0, 1), (0, 2)]
   >>> allocateEdges(graph, edges)
+  >>> graph  # allocateEdges modifies in-place
   [[1, 2], [], [], []]
   """
   for a, b in edges:
     graph[a].append(b)
-  return graph
+
+
+def generateNextFrontier(nodes, current_depth, distances):
+  """
+  >>> generateNextFrontier([0, 1], 1, [-1, -1])
+  [(0, 2), (1, 2)]
+
+  Now let's test that it will not add already visited nodes to the frontier.
+  In this case, we have already visited the second node so it shouldn't be
+  added to the frontier again.
+  >>> generateNextFrontier([0, 1], 1, [-1, 6])
+  [(0, 2)]
+  """
+  return [(i, current_depth + 1)
+          for i in nodes if distances[i] == -1]
+
+
+def markShortestDistance(distances, node_index, depth):
+  """
+  >>> distances = [-1, -1, -1]
+  >>> node = 1
+  >>> markShortestDistance(distances, node, 2)  # does in-place
+  >>> distances
+  [-1, 12, -1]
+
+  Now let's try to mark a distance that is longer. It should ignore it.
+  >>> markShortestDistance(distances, node, 3)
+  >>> distances
+  [-1, 12, -1]
+
+  Now let's mark a shorter distance, which should replace the current shortest.
+  >>> markShortestDistance(distances, node, 1)
+  >>> distances
+  [-1, 6, -1]
+  """
+  current = distances[node_index]
+  distance = depth * WEIGHT
+
+  if current == -1:  # If it's never been measured before, set it
+    distances[node_index] = distance
+  else:
+    distances[node_index] = min(distances[node_index], distance)
 
 
 def explore(graph, S):
   """Explores the graph, compiling a list of lengths to each node.
   >>> graph = [[1, 2], [], [], []]
   >>> S = 0
-  >>> r = explore(graph, S)
-
-  The length of the array returned should be one less than
-  the number of nodes in the graph.
-  >>> len(r) == (len(graph) - 1)
-  True
-
-  And the result should be this.
-  >>> r == [6, 6, -1]
-  True
+  >>> explore(graph, S)
+  [0, 6, 6, -1]
   """
-  return []
+
+  # Initialize the distances array, all -1 at first, meaning no path.
+  # We'll discover them soon.
+  distances = initArray(len(graph), -1)
+  distances[S] = 0  # The node has a distance of 0 from itself.
+
+  # Initialize the frontier with nodes that are reachable from
+  # the beginning node S.
+  frontier = generateNextFrontier(graph[S], 0, distances)
+
+  while len(frontier):
+    node, depth = frontier.pop()
+    markShortestDistance(distances, node, depth)
+    frontier.extend(generateNextFrontier(graph[node], depth, distances))
+
+  return distances
 
 
 def solve(N, edges, S):
   """
   >>> edges = [(0, 1), (0, 2)]
   >>> solve(4, edges, 0)
-  6 6 -1
+  [6, 6, -1]
   """
 
   graph = initArray(N)
   allocateEdges(graph, edges)
+  result = explore(graph, S)
+
+  # According to the output specs for this problem, we don't return the
+  # distance from the starting element to itself, so let's remove it.
+  del result[S]
+
+  return result
 
 
 def main():
-  pass
+  T = int(raw_input())
+
+  for t in range(T):
+    N, M = map(int, raw_input().split())
+    edges = []
+    for m in range(M):
+      x, y = map(int, raw_input().split())
+      edges.append((x-1, y-1))
+    S = int(raw_input()) - 1
+    result = solve(N, edges, S)
+    for d in result:
+      print d,
+    print
 
 
 if __name__ == '__main__':
